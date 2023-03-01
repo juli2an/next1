47e52566662667f85f55e14a76594f2757780829
juli2an <126669477+juli2an@users.noreply.github.com>
2023-03-01 13:16:17 +0100

    Commit inicial

---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+import styles from '@/styles/Home.module.css'
+
+const Blog = () => {
+  return (
+    <main className={styles.main}>
+    <div>
+      <h1>BLOG</h1>
+     </div>
+   </main>
+  )
+}
+
+export default Blog
